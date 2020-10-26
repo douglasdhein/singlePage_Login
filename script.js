3c26ea3d43ad579e34f317db88ae9e7647b4c76a
= <=>
2020-10-26 12:35:26 -0300

    Adicionando primeira parte da singlepage

---
+++ script.js
@@ -0,0 +1,16 @@
+function efetuarLogin(){
+    document.getElementById("containerLogin").style.display = "none";
+    document.getElementById("containerHome").style.display = "block";
+
+}
+
+function efetuarLogout(){
+    document.getElementById("containerHome").style.display = "none";
+    document.getElementById("containerLogin").style.display = "block";
+}
+
+
+
+
+
+
